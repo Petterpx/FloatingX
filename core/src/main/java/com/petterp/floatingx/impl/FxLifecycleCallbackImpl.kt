299811605137b6253351c333bdb9973917bff567
petterp <shiyihuicloud@163.com>
2021-06-10 18:58:07 +0800

    fix:修复Android9.0以下出现的topActivity内存泄漏与部分回调未执行

--- core/src/main/java/com/petterp/floatingx/impl/FxLifecycleCallbackImpl.kt
+++ core/src/main/java/com/petterp/floatingx/impl/FxLifecycleCallbackImpl.kt
@@ -7,6 +7,7 @@ import com.petterp.floatingx.config.FxHelper
 import com.petterp.floatingx.ext.FxDebug
 import com.petterp.floatingx.ext.fxParentView
 import com.petterp.floatingx.listener.IFxControl
+import java.lang.ref.WeakReference
 
 /**
  * @Author petterp
@@ -19,7 +20,7 @@ class FxLifecycleCallbackImpl(
 ) :
     Application.ActivityLifecycleCallbacks {
     internal var control: IFxControl? = null
-    internal var topActivity: Activity? = null
+    internal var topActivity: WeakReference<Activity>? = null
 
     private val Activity.isParent: Boolean
         get() = control?.getView()?.parent === fxParentView
@@ -36,7 +37,7 @@ class FxLifecycleCallbackImpl(
     }
 
     override fun onActivityStarted(activity: Activity) {
-        topActivity = activity
+        initActivity(activity)
         FxDebug.d("AppLifecycle--[${activity.name}]-onActivityStarted")
         helper.fxLifecycleExpand?.onActivityStarted?.let {
             if (activity.isActivityInValid) it.invoke(activity)
@@ -44,9 +45,10 @@ class FxLifecycleCallbackImpl(
     }
 
     override fun onActivityResumed(activity: Activity) {
+        initActivity(activity)
+        FxDebug.d("AppLifecycle--[${activity.name}]-onActivityResumed")
         val isActivityInValid = activity.isActivityInValid
         val isParent = activity.isParent
-        FxDebug.d("AppLifecycle--[${activity.name}]-onActivityResumed")
         FxDebug.d("view->isAttach? isContainActivity-$isActivityInValid--enableFx-${helper.enableFx}---isParent-$isParent")
         if (helper.enableFx && isActivityInValid && !isParent)
             control?.attach(activity)
@@ -69,105 +71,18 @@ class FxLifecycleCallbackImpl(
         }
     }
 
-    override fun onActivityPostDestroyed(activity: Activity) {
-        if (topActivity === activity) topActivity = null
-        val isParent = activity.isParent
-        FxDebug.d("AppLifecycle--[${activity.name}]-onActivityPostDestroyed")
-        FxDebug.d("view->isDetach? isContainActivity-${activity.isActivityInValid}--enableFx-${helper.enableFx}---isParent-$isParent")
-        if (helper.enableFx && isParent)
-            control?.detach(activity)
-        helper.fxLifecycleExpand?.onActivityPostDestroyed?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
     override fun onActivityDestroyed(activity: Activity) {
-        FxDebug.d("AppLifecycle--[${activity.name}]-onActivityDestroyed")
         helper.fxLifecycleExpand?.onActivityDestroyed?.let {
             if (activity.isActivityInValid) it.invoke(activity)
         }
-    }
-
-    override fun onActivityPostPaused(activity: Activity) {
-        helper.fxLifecycleExpand?.onActivityPostPaused?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPostStopped(activity: Activity) {
-        helper.fxLifecycleExpand?.onActivityPostStopped?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPreDestroyed(activity: Activity) {
-        helper.fxLifecycleExpand?.onActivityPreDestroyed?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPreCreated(activity: Activity, savedInstanceState: Bundle?) {
-        helper.fxLifecycleExpand?.onActivityPreCreated?.let {
-            if (activity.isActivityInValid) it.invoke(activity, savedInstanceState)
-        }
-    }
-
-    override fun onActivityPrePaused(activity: Activity) {
-        super.onActivityPrePaused(activity)
-        helper.fxLifecycleExpand?.onActivityPrePaused?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPreResumed(activity: Activity) {
-        super.onActivityPreResumed(activity)
-        helper.fxLifecycleExpand?.onActivityPreResumed?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPostSaveInstanceState(activity: Activity, outState: Bundle) {
-        helper.fxLifecycleExpand?.onActivityPostSaveInstanceState?.let {
-            if (activity.isActivityInValid) it.invoke(activity, outState)
-        }
-    }
-
-    override fun onActivityPreSaveInstanceState(activity: Activity, outState: Bundle) {
-        super.onActivityPreSaveInstanceState(activity, outState)
-        helper.fxLifecycleExpand?.onActivityPreSaveInstanceState?.let {
-            if (activity.isActivityInValid) it.invoke(activity, outState)
-        }
-    }
-
-    override fun onActivityPreStopped(activity: Activity) {
-        super.onActivityPreStopped(activity)
-        helper.fxLifecycleExpand?.onActivityPreStopped?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPostResumed(activity: Activity) {
-        topActivity = activity
-        helper.fxLifecycleExpand?.onActivityPostResumed?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPostStarted(activity: Activity) {
-        helper.fxLifecycleExpand?.onActivityPostStarted?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPreStarted(activity: Activity) {
-        helper.fxLifecycleExpand?.onActivityPreStarted?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
-        }
-    }
-
-    override fun onActivityPostCreated(activity: Activity, savedInstanceState: Bundle?) {
-        helper.fxLifecycleExpand?.onActivityPostCreated?.let {
-            if (activity.isActivityInValid) it.invoke(activity)
+        val isParent = activity.isParent
+        FxDebug.d("AppLifecycle--[${activity.name}]-onActivityDestroyed")
+        FxDebug.d("view->isDetach? isContainActivity-${activity.isActivityInValid}--enableFx-${helper.enableFx}---isParent-$isParent")
+        if (helper.enableFx && isParent)
+            control?.detach(activity)
+        if (topActivity?.get() === activity) {
+            topActivity?.clear()
+            topActivity = null
         }
     }
 
@@ -175,5 +90,10 @@ class FxLifecycleCallbackImpl(
         helper.fxLifecycleExpand?.onActivitySaveInstanceState?.let {
             if (activity.isActivityInValid) it.invoke(activity, outState)
         }
+    }
+
+    private fun initActivity(activity: Activity) {
+        if (topActivity?.get() != activity)
+            topActivity = WeakReference(activity)
     }
 }
