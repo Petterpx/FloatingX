5c10cf2ad67450df08be01a6a6e96d95e60d6a10
petterp <shiyihuicloud@163.com>
2022-10-26 17:52:02 +0800

    doc:增加注释

--- floatingx/src/main/java/com/petterp/floatingx/assist/helper/ScopeHelper.kt
+++ floatingx/src/main/java/com/petterp/floatingx/assist/helper/ScopeHelper.kt
@@ -10,12 +10,15 @@ import com.petterp.floatingx.listener.control.IFxScopeControl
 /** 特定范围的Helper构建器 */
 class ScopeHelper : BasisHelper() {
 
+    /** 插入到Activity中 */
     fun toControl(activity: Activity): IFxControl =
         toControl().init(activity)
 
+    /** 插入到Fragment中 */
     fun toControl(fragment: Fragment): IFxControl =
         toControl().init(fragment)
 
+    /** 插入到ViewGroup中 */
     fun toControl(group: ViewGroup): IFxControl =
         toControl().init(group)
 
