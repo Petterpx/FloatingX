46c044b21bed39654819ef34c4bd5d769fe7b906
petterp <shiyihuicloud@163.com>
2023-01-01 09:33:19 +0800

    simple:调整示例

--- app/src/main/java/com/petterp/floatingx/app/MainActivity.kt
+++ app/src/main/java/com/petterp/floatingx/app/MainActivity.kt
@@ -32,63 +32,71 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         createLinearLayoutToParent {
             viewGroup = addScopeFrameViewGroup()
-            addItemView("隐藏全局悬浮窗") {
-                FloatingX.control().hide()
-            }
-            addItemView("显示全局悬浮窗-(展示与多指触摸)") {
-                // 虽然可以不传递activity,不传递时将使用当前栈顶activity
+            addNestedScrollView {
+                addLinearLayout {
+                    addItemView("显示全局悬浮窗") {
+                        FloatingX.control().show()
+                    }
+                    addItemView("隐藏全局悬浮窗") {
+                        FloatingX.control().hide()
+                    }
+                    addItemView("显示全局悬浮窗-(展示与多指触摸)") {
+                        // 虽然可以不传递activity,不传递时将使用当前栈顶activity
 //                FloatingX.control().show(this@MainActivity)
-                FloatingX.control().show()
-            }
-            addItemView("进入黑名单页面(禁止显示浮窗)") {
-                BlackActivity::class.java.start(context)
-            }
-            addItemView("更新当前全局浮窗显示View-(layoutId)") {
-                FloatingX.control().apply {
-                    updateView(R.layout.item_floating)
-                    this.updateViewContent {
-                        it.setText(R.id.tvItemFx, "App")
+                        FloatingX.control().show()
                     }
-                }.show()
-            }
-            addItemView("更新当前全局浮窗显示View-(layoutView)") {
-                FloatingX.control().updateView {
-                    TextView(it).apply {
-                        layoutParams = ViewGroup.LayoutParams(
-                            ViewGroup.LayoutParams.WRAP_CONTENT,
-                            ViewGroup.LayoutParams.WRAP_CONTENT
-                        )
-                        text = "App"
-                        textSize = 15f
-                        setBackgroundColor(Color.GRAY)
-                        setPadding(10, 10, 10, 10)
+                    addItemView("进入黑名单页面(禁止显示浮窗)") {
+                        BlackActivity::class.java.start(context)
                     }
-                }
-                FloatingX.control().show()
-            }
-            addItemView("显示Activity悬浮窗-(展示与多指触摸)") {
-                activityFx.show()
-                activityFx.updateViewContent {
-                    it.setText(R.id.tvItemFx, "Act")
-                    it.getView<CardView>(R.id.cardItemFx).setCardBackgroundColor(Color.BLUE)
-                }
-            }
-            addItemView("显示View级别悬浮窗-(展示与多指触摸)") {
-                viewFx.show()
-                viewFx.updateViewContent {
-                    it.setText(R.id.tvItemFx, "view")
-                    it.getView<CardView>(R.id.cardItemFx).setCardBackgroundColor(Color.GREEN)
-                }
-            }
-            addItemView("调整到无状态栏页面-(测试状态栏影响)") {
-                ImmersedActivity::class.java.start(context)
-            }
-            addItemView("跳转到局部悬浮窗页面-(测试api功能)") {
-                ScopeActivity::class.java.start(context)
-            }
+                    addItemView("更新当前全局浮窗显示View-(layoutId)") {
+                        FloatingX.control().apply {
+                            updateView(R.layout.item_floating)
+                            this.updateViewContent {
+                                it.setText(R.id.tvItemFx, "App")
+                            }
+                        }.show()
+                    }
+                    addItemView("更新当前全局浮窗显示View-(layoutView)") {
+                        FloatingX.control().updateView {
+                            TextView(it).apply {
+                                layoutParams = ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                                    ViewGroup.LayoutParams.WRAP_CONTENT
+                                )
+                                text = "App"
+                                textSize = 15f
+                                setBackgroundColor(Color.GRAY)
+                                setPadding(10, 10, 10, 10)
+                            }
+                        }
+                        FloatingX.control().show()
+                    }
+                    addItemView("显示Activity悬浮窗-(展示与多指触摸)") {
+                        activityFx.show()
+                        activityFx.updateViewContent {
+                            it.setText(R.id.tvItemFx, "Act")
+                            it.getView<CardView>(R.id.cardItemFx).setCardBackgroundColor(Color.BLUE)
+                        }
+                    }
+                    addItemView("显示View级别悬浮窗-(展示与多指触摸)") {
+                        viewFx.show()
+                        viewFx.updateViewContent {
+                            it.setText(R.id.tvItemFx, "view")
+                            it.getView<CardView>(R.id.cardItemFx)
+                                .setCardBackgroundColor(Color.GREEN)
+                        }
+                    }
+                    addItemView("调整到无状态栏页面-(测试状态栏影响)") {
+                        ImmersedActivity::class.java.start(context)
+                    }
+                    addItemView("跳转到局部悬浮窗页面-(测试api功能)") {
+                        ScopeActivity::class.java.start(context)
+                    }
 //            addItemView("跳转到测试页面-(测试申请权限的浮窗)") {
 //                SingleActivity::class.java.start(context)
 //            }
+                }
+            }
         }
     }
 
