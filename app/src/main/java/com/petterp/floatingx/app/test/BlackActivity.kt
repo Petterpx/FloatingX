595c32ac6e8be3a5074242e6c816398451527687
petterp <shiyihuicloud@163.com>
2023-11-10 00:09:06 +0800

    simple:调整示例

--- app/src/main/java/com/petterp/floatingx/app/test/BlackActivity.kt
+++ app/src/main/java/com/petterp/floatingx/app/test/BlackActivity.kt
@@ -2,6 +2,8 @@ package com.petterp.floatingx.app.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.petterp.floatingx.FloatingX
+import com.petterp.floatingx.app.addItemView
 import com.petterp.floatingx.app.addTextView
 import com.petterp.floatingx.app.createLinearLayoutToParent
 
@@ -13,6 +15,9 @@ class BlackActivity : AppCompatActivity() {
             addTextView {
                 text = "我是黑名单页面"
                 textSize = 50f
+            }
+            addItemView("显示全局悬浮窗") {
+                FloatingX.control(MultipleFxActivity.TAG_1).show(this@BlackActivity)
             }
         }
     }
