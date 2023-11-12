63ad0bed18166b204ca34cdc9cb2792feb006bd2
petterp <shiyihuicloud@163.com>
2023-11-12 17:20:28 +0800

    simple:调整示例

--- app/src/main/java/com/petterp/floatingx/app/kotlin/CustomKtApplication.kt
+++ app/src/main/java/com/petterp/floatingx/app/kotlin/CustomKtApplication.kt
@@ -67,7 +67,7 @@ class CustomKtApplication : Application() {
                 // 启用辅助方向,具体参加方法注释
                 setEnableAssistDirection(r = 100f)
                 // 设置启用边缘吸附,默认启用
-                setEnableEdgeAdsorption(false)
+                setEnableEdgeAdsorption(true)
                 // 设置边缘偏移量
                 setEdgeOffset(10f)
                 // 设置启用悬浮窗可屏幕外回弹
